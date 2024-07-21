bf86da72830945cd32eb65fec8d4061eee47fae1
Komla Roland GOLO <36695969+venusky@users.noreply.github.com>
2024-07-21 23:08:04 +0000

    corriger setPasword.tsx

--- src/pages/api/v1/recovery/setPassword.tsx
+++ src/pages/api/v1/recovery/setPassword.tsx
@@ -1,6 +1,8 @@
 import {NextApiRequest, NextApiResponse} from "next";
 import Cors from "nextjs-cors";
 
+const URL = process.env.URL_FRONT
+
 export default async function handler(req:NextApiRequest, res:NextApiResponse){
     await Cors(req, res, {
         methods: ['GET', 'POST', 'PUT'],
