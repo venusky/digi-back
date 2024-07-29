fe658ccdadaf6713318818e05dca554877448ad0
Komla Roland GOLO <36695969+venusky@users.noreply.github.com>
2024-07-29 10:59:21 +0000

    update a fetch method from asc to desc

--- src/pages/api/v1/commande/index.tsx
+++ src/pages/api/v1/commande/index.tsx
@@ -35,7 +35,7 @@ export default async function handler (req:NextApiRequest, res:NextApiResponse){
                         prelevement: true
                     }
                     } },
-                orderBy: { createdAt: "asc" },
+                orderBy: { createdAt: "desc" },
             })
             if (commandes){
                 return res.status(200).json({
